afc9da46660bd5a767c750c089cc13dbd35675cc
mit-ryo <cs.cayley061@docomo.ne.jp>
2018-12-21 21:44:52 +0900

    18/12/21

--- login/sql/struts.sql
+++ login/sql/struts.sql
@@ -1,6 +1,6 @@
 set names utf8;
 set foreign_key_checks = 0;
-drop database if exists logindb mitsuhashi;
+drop database if exists logindb_mitsuhashi;
 create database logindb_mitsuhashi;
 use logindb_mitsuhashi;
 
